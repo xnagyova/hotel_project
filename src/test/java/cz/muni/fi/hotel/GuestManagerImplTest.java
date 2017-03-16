e3a4f8997b4cae62b33176af73ceb9d5c4496e23
Silvia Nagyova <silvianagyova.sn@ggmail.com>
2017-03-16 22:58:47 +0100

    Guest hotovo

--- src/test/java/cz/muni/fi/hotel/GuestManagerImplTest.java
+++ src/test/java/cz/muni/fi/hotel/GuestManagerImplTest.java
@@ -43,7 +43,7 @@ public class GuestManagerImplTest {
     private GuestBuilder sampleJohnGuestBuilder() {
         return new GuestBuilder()
                 .name("John Fox")
-                .dateOfBirth(1984,Month.FEBRUARY,5)
+                .dateOfBirth(1980,Month.APRIL,22)
                 .phoneNumber("+421947865586");
 
     }
@@ -54,6 +54,13 @@ public class GuestManagerImplTest {
                 .name("Samantha Fox")
                 .dateOfBirth(1974,Month.AUGUST,10)
                 .phoneNumber("+421947842396");
+
+    }
+    private GuestBuilder sampleSamantha2GuestBuilder() {
+        return new GuestBuilder()
+                .name("Samantha Fox")
+                .dateOfBirth(1975,Month.NOVEMBER,21)
+                .phoneNumber("+421947741366");
 
     }
 
@@ -82,37 +89,24 @@ public class GuestManagerImplTest {
     public void createGuestBornTomorrow() {
 
         LocalDate tomorrow = TODAY.toLocalDate().plusDays(1);
-        Guest guest = newGuest("John Legend", tomorrow, "+421974889962");
-
-        guestManager.createGuest(guest);
-
+        Guest guest =sampleJohnGuestBuilder()
+                .dateOfBirth(tomorrow.getYear(),tomorrow.getMonth(),tomorrow.getDayOfMonth())
+                .build();
         expectedException.expect(ValidationException.class);
         guestManager.createGuest(guest);
     }
 
     @Test
     public void createGuestBornToday() {
-
-
-        Guest guest = newGuest("John Legend", TODAY.toLocalDate(), "+421974889962");
-
+        Guest guest = sampleJohnGuestBuilder()
+                .dateOfBirth(TODAY.getYear(),TODAY.getMonth(),TODAY.getDayOfMonth())
+                .build();
         guestManager.createGuest(guest);
 
-        Long guestId = guest.getId();
-        assertNotNull(guestId);
-        Guest result = guestManager.findGuestById(guestId);
-        assertEquals(guest, result);
-        assertNotSame(guest, result);
-        assertDeepEquals(guest, result);
-    }
+        assertThat(guestManager.findGuestById(guest.getId()))
+                .isNotNull()
+                .isEqualToComparingFieldByField(guest);
 
-
-    @Test
-    public void createGuestWithExistingId() {
-        Guest guest = newGuest("Peter Novák", LocalDate.of(1970, 11, 11), "+420555666777");
-        guest.setId(1L);
-        expectedException.expect(IllegalArgumentException.class);
-        guestManager.createGuest(guest);
     }
 
 
@@ -127,31 +121,21 @@ public class GuestManagerImplTest {
 
 
     @Test
-    public void createGuestWithNullDateOfBirth() {
-        Guest guest = newGuest("John Fork", null, "+42199977788");
-        guestManager.createGuest(guest);
-        Guest result = guestManager.findGuestById(guest.getId());
-        assertNotNull(result);
-        assertNotNull(result.getDateOfBirth());
-    }
-
-    @Test
-    public void createGuestWithNonExistingMonthOfBirth() {
-        Guest guest = newGuest("John Fork", LocalDate.of(1998,13,1), "+42199977788");
-        expectedException.expect(IllegalArgumentException.class);
-        guestManager.createGuest(guest);
-    }
-
-    @Test
     public void createGuestWithNonExistingYearOfBirth() {
-        Guest guest = newGuest("John Fork", LocalDate.of(-1,12,1), "+42199977788");
+        Guest guest = sampleJohnGuestBuilder()
+                .dateOfBirth(-1,Month.DECEMBER,1)
+                .build();
+
         expectedException.expect(IllegalArgumentException.class);
         guestManager.createGuest(guest);
     }
 
     @Test
     public void createGuestWithNonExistingDayOfBirth() {
-        Guest guest = newGuest("John Fork", LocalDate.of(1998,2,30), "+42199977788");
+        Guest guest = sampleJohnGuestBuilder()
+                .dateOfBirth(1998,Month.FEBRUARY,32)
+                .build();
+
         expectedException.expect(IllegalArgumentException.class);
         guestManager.createGuest(guest);
     }
@@ -160,16 +144,20 @@ public class GuestManagerImplTest {
 
     @Test
     public void createGuestWithNullPhoneNumber() {
-        Guest guest = newGuest("Jozef Celer",LocalDate.of(1970,11,11),null);
-        guestManager.createGuest(guest);
-        Guest result = guestManager.findGuestById(guest.getId());
-        assertNotNull(result);
-        assertNotNull(result.getPhoneNumber());
+        Guest guest = sampleJohnGuestBuilder()
+                .phoneNumber(null)
+                .build();
+        assertThatThrownBy(() -> guestManager.createGuest(guest))
+                .isInstanceOf(ValidationException.class);
     }
 
     @Test
     public void createGuestWithWrongPhoneNumber() {
-        Guest guest = newGuest("Jozef Celer", LocalDate.of(1970,11,11),"+12a871654");
+
+        Guest guest = sampleJohnGuestBuilder()
+                .phoneNumber("+12a871654")
+                .build();
+
         expectedException.expect(IllegalArgumentException.class);
         guestManager.createGuest(guest);
     }
@@ -180,18 +168,18 @@ public class GuestManagerImplTest {
     @Test
     public void deleteGuest() {
 
-        Guest g1 = newGuest( "Radoslav Bob",LocalDate.of(1977,11,11),"+421911222555");
-        Guest g2 = newGuest("Radoslava Boba",LocalDate.of(1978,12,12),"+421911444555");
-        guestManager.createGuest(g1);
-        guestManager.createGuest(g2);
+        Guest john = sampleJohnGuestBuilder().build();
+        Guest samantha = sampleSamanthaGuestBuilder().build();
 
-        assertNotNull(guestManager.findGuestById(g1.getId()));
-        assertNotNull(guestManager.findGuestById(g2.getId()));
+        guestManager.createGuest(john);
+        guestManager.createGuest(samantha);
 
-        guestManager.deleteGuest(g1);
+        assertThat(guestManager.findGuestById(john.getId())).isNotNull();
+        assertThat(guestManager.findGuestById(samantha.getId())).isNotNull();
 
-        assertNull(guestManager.findGuestById(g1.getId()));
-        assertNotNull(guestManager.findGuestById(g2.getId()));
+        guestManager.deleteGuest(john);
+        assertThat(guestManager.findGuestById(john.getId())).isNull();
+        assertThat(guestManager.findGuestById(samantha.getId())).isNotNull();
 
     }
 
@@ -204,39 +192,44 @@ public class GuestManagerImplTest {
 
     @Test
     public void updateGuestInformation() {
-        Guest guest = newGuest("Peter Tamten",LocalDate.of(1977,11,11),"+420111888777");
-        Guest anotherGuest = newGuest("Petra Tamta",LocalDate.of(1988,12,12), "+421944552558");
-        guestManager.createGuest(guest);
-        guestManager.createGuest(anotherGuest);
-        Long guestId = guest.getId();
+        Guest guestToUpdate = sampleJohnGuestBuilder().build();
+        Guest samantha = sampleSamanthaGuestBuilder().build();
+        guestManager.createGuest(guestToUpdate);
+        guestManager.createGuest(samantha);
 
-        guest = guestManager.findGuestById(guestId);
-        guest.setName("Pavol Bežný");
-        guestManager.updateGuestInformation(guest);
-        assertEquals("Pavol Bežný",guest.getName());
-        assertEquals(LocalDate.of(1977,11,11),guest.getDateOfBirth());
-        assertEquals("+420111888777", guest.getPhoneNumber());
+        guestToUpdate.setName("Peter Clock");
 
-        guest = guestManager.findGuestById(guestId);
-        guest.setDateOfBirth(LocalDate.of(1987,11,11));
-        guestManager.updateGuestInformation(guest);
-        assertEquals("Pavol Bežný",guest.getName());
-        assertEquals(LocalDate.of(1987,11,11),guest.getDateOfBirth());
-        assertEquals("+420111888777", guest.getPhoneNumber());
+        guestManager.updateGuestInformation(guestToUpdate);
 
-        guest = guestManager.findGuestById(guestId);
-        guest.setPhoneNumber("+420777777777");
-        guestManager.updateGuestInformation(guest);
-        assertEquals("Pavol Bežný",guest.getName());
-        assertEquals(LocalDate.of(1987,11,11),guest.getDateOfBirth());
-        assertEquals("+420777777777", guest.getPhoneNumber());
+        assertThat(guestManager.findGuestById(guestToUpdate.getId()))
+                .isEqualToComparingFieldByField(guestToUpdate);
+
+        assertThat(guestManager.findGuestById(samantha.getId()))
+                .isEqualToComparingFieldByField(samantha);
 
 
 
-        // Check if updates didn't affected other records
-        assertDeepEquals(anotherGuest, guestManager.findGuestById(anotherGuest.getId()));
+    }
 
+    @FunctionalInterface
+    private static interface Operation<T> {
+        void callOn(T subjectOfOperation);
+    }
 
+    private void testUpdateGuestInformation(GuestManagerImplTest.Operation<Guest> updateOperation) {
+        Guest john = sampleJohnGuestBuilder().build();
+        Guest samantha = sampleSamanthaGuestBuilder().build();
+        guestManager.createGuest(john);
+        guestManager.createGuest(john);
+
+        updateOperation.callOn(john);
+
+        guestManager.updateGuestInformation(john);
+        assertThat(guestManager.findGuestById(john.getId()))
+                .isEqualToComparingFieldByField(john);
+
+        assertThat(guestManager.findGuestById(samantha.getId()))
+                .isEqualToComparingFieldByField(samantha);
     }
 
 
@@ -245,14 +238,33 @@ public class GuestManagerImplTest {
         guestManager.updateGuestInformation(null);
     }
 
+    @Test
+    public void updateGuestDateOfBirth() {
+        testUpdateGuestInformation((guest) -> guest.setDateOfBirth(LocalDate.of(1999,Month.DECEMBER,12)));
+    }
+
+    @Test
+    public void updateGuestPhoneNumber() {
+        testUpdateGuestInformation((guest) -> guest.setPhoneNumber("+421947362584"));
+
+    }
+
+    @Test
+    public void updateNonExistingGuest() {
+        Guest guest = sampleJohnGuestBuilder().id(1L).build();
+        expectedException.expect(IllegalEntityException.class);
+        guestManager.updateGuestInformation(guest);
+    }
+
 
 
     @Test
     public void updateGuestWithWrongPhoneNumber() {
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().phoneNumber("+420777125551").build();
         guestManager.createGuest(guest);
         guest.setPhoneNumber("+420a12456789");
-        expectedException.expect(IllegalArgumentException.class);
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
     }
 
@@ -261,19 +273,21 @@ public class GuestManagerImplTest {
 
     @Test
     public void updateGuestWithNullPhoneNumber() {
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().build();
         guestManager.createGuest(guest);
         guest.setPhoneNumber(null);
-        expectedException.expect(IllegalArgumentException.class);
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
     }
 
     @Test
     public void updateGuestWithNullDateOfBirth() {
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().build();
         guestManager.createGuest(guest);
         guest.setDateOfBirth(null);
-        expectedException.expect(IllegalArgumentException.class);
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
     }
 
@@ -281,52 +295,53 @@ public class GuestManagerImplTest {
     @Test
     public void updateGuestWithTomorrowDateOfBirth() {
         LocalDate tomorrow = TODAY.toLocalDate().plusDays(1);
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().dateOfBirth(1998,Month.APRIL,15).build();
         guestManager.createGuest(guest);
         guest.setDateOfBirth(tomorrow);
-        expectedException.expect(IllegalArgumentException.class);
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
     }
 
     @Test
     public void updateGuestWithWrongDayOfBirth() {
-
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().dateOfBirth(1998,Month.APRIL,15).build();
         guestManager.createGuest(guest);
         guest.setDateOfBirth(LocalDate.of(1998,11,32));
-        expectedException.expect(IllegalArgumentException.class);
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
+
     }
 
     @Test
     public void updateGuestWithWrongMonthOfBirth() {
-
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().dateOfBirth(1998,Month.APRIL,15).build();
         guestManager.createGuest(guest);
-        guest.setDateOfBirth(LocalDate.of(1998,13,2));
-        expectedException.expect(IllegalArgumentException.class);
+        guest.setDateOfBirth(LocalDate.of(1998,13,32));
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
     }
 
     @Test
     public void updateGuestWithWrongYearOfBirth() {
-
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().dateOfBirth(1998,Month.APRIL,15).build();
         guestManager.createGuest(guest);
-        guest.setDateOfBirth(LocalDate.of(-1,10,2));
-        expectedException.expect(IllegalArgumentException.class);
+        guest.setDateOfBirth(LocalDate.of(-1,11,32));
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
     }
 
     @Test
     public void updateGuestWithNullName() {
-        Guest guest = newGuest("Michal Pekný",LocalDate.of(1998,5,15),"+420777125551");
+        Guest guest = sampleJohnGuestBuilder().build();
         guestManager.createGuest(guest);
         guest.setName(null);
-        Guest result = guestManager.findGuestById(guest.getId());
+
+        expectedException.expect(ValidationException.class);
         guestManager.updateGuestInformation(guest);
-        assertNotNull(result);
-        assertNotNull(result.getPhoneNumber());
     }
 
 
@@ -356,70 +371,37 @@ public class GuestManagerImplTest {
     public void findGuestById() {
 
 
-        Guest guest = newGuest("Peter Celer",LocalDate.of(1977,11,11),"+420111444777");
-        Guest guest2 = newGuest("Peter Celer",LocalDate.of(1975,11,15),"+421987566889");
-        guestManager.createGuest(guest);
-        guestManager.createGuest(guest2);
-        Guest result = guestManager.findGuestById(guest.getId());
-        assertEquals(guest,result);
-        assertNotEquals(guest2,result);
-        assertDeepEquals(guest,result);
+        Guest john = sampleJohnGuestBuilder().build();
+        Guest samantha = sampleSamanthaGuestBuilder().build();
+
+        guestManager.createGuest(john);
+        guestManager.createGuest(samantha);
+
+        assertThat(guestManager.findGuestById(john.getId()))
+                .usingComparatorForFields(GUEST_ID_COMPARATOR)
+                .isSameAs(john);
 
     }
 
     @Test
     public void findGuestByName() throws Exception {
 
-        assertTrue(guestManager.findAllGuests().isEmpty());
+        assertThat(guestManager.findAllGuests()).isEmpty();
 
-        Guest g1 = newGuest("Pavol Rychlý",LocalDate.of(1977,11,11),"+42193555777");
-        Guest g2 = newGuest("Petra Bystrá",LocalDate.of(1978,11,11),"+420777888999");
-        Guest g3 = newGuest("Petra Bystrá",LocalDate.of(1965,11,22),"+420333222111");
-        Guest g4 = newGuest("Petra Bystrá",LocalDate.of(1973,11,7),"+420111999777");
-        Guest g5 = newGuest("Petra Bystrá",LocalDate.of(1961,11,13),"+420777888259");
+        Guest john = sampleJohnGuestBuilder().build();
+        Guest samantha = sampleSamanthaGuestBuilder().build();
+        Guest anotherSamantha = sampleSamantha2GuestBuilder().build();
 
-        guestManager.createGuest(g1);
-        guestManager.createGuest(g2);
-        guestManager.createGuest(g3);
-        guestManager.createGuest(g4);
-        guestManager.createGuest(g5);
+        guestManager.createGuest(john);
+        guestManager.createGuest(samantha);
+        guestManager.createGuest(anotherSamantha);
 
-        List<Guest> expected = Arrays.asList(g2,g3, g4, g5);
-        List<Guest> actual = guestManager.findGuestByName(g2.getName());
-
-        actual.sort(GUEST_ID_COMPARATOR);
-        expected.sort(GUEST_ID_COMPARATOR);
-
-        assertEquals(expected,actual);
-        assertDeepEquals(expected,actual);
+        assertThat(guestManager.findGuestByName(samantha.getName()))
+                .usingFieldByFieldElementComparator()
+                .containsOnly(samantha,anotherSamantha);
 
     }
 
-    private void assertDeepEquals(List<Guest> expectedList, List<Guest> actualList) {
-        for (int i = 0; i < expectedList.size(); i++) {
-            Guest expected = expectedList.get(i);
-            Guest actual = actualList.get(i);
-            assertDeepEquals(expected, actual);
-        }
-    }
-
-
-    private void assertDeepEquals(Guest expected, Guest actual) {
-
-        assertEquals(expected.getId(), actual.getId());
-        assertEquals(expected.getDateOfBirth(), actual.getDateOfBirth());
-        assertEquals(expected.getName(), actual.getName());
-        assertEquals(expected.getPhoneNumber(), actual.getPhoneNumber());
-
-    }
-
-    private static Guest newGuest(String name, LocalDate dateOfBirth, String phoneNumber) {
-        Guest guest = new Guest();
-        guest.setDateOfBirth(dateOfBirth);
-        guest.setName(name);
-        guest.setPhoneNumber(phoneNumber);
-        return guest;
-    }
 
     private static final Comparator<Guest> GUEST_ID_COMPARATOR =
             (g1, g2) -> g1.getId().compareTo(g2.getId());
